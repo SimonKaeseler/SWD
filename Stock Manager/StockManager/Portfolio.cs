a0058cc3b6ccf24e53894e5075b615308e4f0fba
SimonKaeseler <simonkaeseler@gmail.com>
2016-02-25 14:55:37 +0100

    Stock + Portfolio.cs

---
+++ Stock Manager/StockManager/Portfolio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace StockManager
+{
+    public class Portfolio
+    {
+        private List<Stock> _myStockList;
+
+        public Portfolio()
+        {
+            _myStockList = new List<Stock>();
+        }
+    }
+}
---
+++ Stock Manager/StockManager/Stock.cs
@@ -0,0 +1,26 @@
+﻿namespace StockManager
+{
+    public class Stock : IObserver
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public uint Amount { get; set; }
+        private readonly StockValue _stockValue;
+
+        public Stock(StockValue stockValue)
+        {
+            _stockValue = stockValue;
+            
+        }
+        /// <summary>
+        /// When ever a StockValue is updated and it notifies, this is called to pull new info
+        /// </summary>
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class StockValue
+    { }
+}
