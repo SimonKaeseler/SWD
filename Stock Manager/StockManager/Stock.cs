f50b1e461d29054c349fc665c9fb7a3dc31a87c3
SimonKaeseler <simonkaeseler@gmail.com>
2016-02-25 15:13:48 +0100

    WPF startet

--- Stock Manager/StockManager/Stock.cs
+++ Stock Manager/StockManager/Stock.cs
@@ -5,22 +5,30 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public uint Amount { get; set; }
-        private readonly StockValue _stockValue;
+        private  StockValue _stockValue;
 
-        public Stock(StockValue stockValue)
+        public Stock()
         {
-            _stockValue = stockValue;
-            
+            _stockValue = new StockValue();
+           
+
         }
         /// <summary>
         /// When ever a StockValue is updated and it notifies, this is called to pull new info
         /// </summary>
         public void Update()
         {
-            throw new System.NotImplementedException();
+            Name = _stockValue.name;
+            Price = _stockValue.price;
+            Amount = _stockValue.amount;
         }
     }
 
     public class StockValue
-    { }
+    {
+        public string name = "";
+        public double price = 0;
+        public uint amount = 0;
+
+    }
 }
